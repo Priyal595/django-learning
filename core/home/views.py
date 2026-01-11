2a144ba27c01943c164bbc69bfc5bd314d5b895d
Priyal Singh <priyalrajput595@gmail.com>
2026-01-11 10:58:59 +0530

    changed funtion url using render

--- core/home/views.py
+++ core/home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return  HttpResponse("<h1>Hi this is the home page</h1>")
+    return  render(request, 'index.html')
+
 
 def success_page(request):
     return HttpResponse("<h1> this is a success page </h1>")
