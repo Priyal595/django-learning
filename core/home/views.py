9406c22d5117893665933fe71b51a6a08a0492a0
Priyal Singh <priyalrajput595@gmail.com>
2026-01-11 10:49:30 +0530

    added html reponses and success page function

--- core/home/views.py
+++ core/home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return  HttpResponse("Hi this is the home page")
+    return  HttpResponse("<h1>Hi this is the home page</h1>")
+
+def success_page(request):
+    return HttpResponse("<h1> this is a success page </h1>")
