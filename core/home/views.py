9174e88f00b5ad4d0eae95187a2ce72141ec4565
Priyal Singh <priyalrajput595@gmail.com>
2026-01-13 22:42:24 +0530

    Adding backend's data to html template

--- core/home/views.py
+++ core/home/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return  render(request, 'index.html')
+    peoples = [
+        {'name' : 'Priyal', 'age': 20},
+        {'name' : 'Priyanshi', 'age': 19},
+        {'name' : 'Ishan', 'age': 16},
+        {'name' : 'Raghav', 'age': 21},
+        {'name' : 'Sudha', 'age': 48},
+    ]
+    return  render(request, 'index.html', context={'peoples': peoples})
 
 
 def success_page(request):
