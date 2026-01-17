e1a42e2f494237043a6a4fe8e2babb0697d45ed5
Priyal Singh <priyalrajput595@gmail.com>
2026-01-17 15:41:09 +0530

    Added the database schema

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    #id = models.AutoField() -> automatically generated
+    name = models.CharField(max_length=100)
+    age  = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null = True ,blank=True )
+    image = models.ImageField()
+    file = models.FileField()
+    
+class Product(models.Model):
+    pass
