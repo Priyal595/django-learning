89a99dc110b39744b062d05ea8416f4eedaffc0c
Priyal Singh <priyalrajput595@gmail.com>
2026-01-18 10:55:58 +0530

    Added a file to test a function using shell

---
+++ core/home/utils.py
@@ -0,0 +1,7 @@
+from .models import Student
+import time 
+#to test a function using shell
+def run_this_function():
+    print("Function started")
+    time.sleep(1)
+    print("function executed")
